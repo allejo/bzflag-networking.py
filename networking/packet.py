6c8e619cb33462ea96f9d50e9b81f6b178aa1c0f
Vladimir Jimenez <allejo@me.com>
2019-01-19 00:34:23 -0800

    Improve packet unpacking

--- networking/packet.py
+++ networking/packet.py
@@ -1,4 +1,5 @@
-from typing import BinaryIO
+from io import BytesIO, BufferedIOBase
+from typing import BinaryIO, Union
 
 from networking.packable_interface import PackableInterface
 
@@ -24,52 +25,57 @@ class Packet(PackableInterface):
         self.data = None
 
     def unpack(self, buf: BinaryIO) -> None:
-        self.mode = Packet.unpack_uint16(buf)
-        self.code = Packet.unpack_uint16(buf)
-        self.length = Packet.unpack_uint32(buf)
-        self.next_file_pos = Packet.unpack_uint32(buf)
-        self.prev_file_pos = Packet.unpack_uint32(buf)
-        self.timestamp = Packet.unpack_int64(buf)
+        chunk = BytesIO(buf.read(32))
+
+        self.mode = Packet.unpack_uint16(chunk)
+        self.code = Packet.unpack_uint16(chunk)
+        self.length = Packet.unpack_uint32(chunk)
+        self.next_file_pos = Packet.unpack_uint32(chunk)
+        self.prev_file_pos = Packet.unpack_uint32(chunk)
+        self.timestamp = Packet.unpack_int64(chunk)
 
         if self.length > 0:
-            self.data = buf.read()
+            self.data = buf.read(self.length)
         else:
             self.data = None
 
     @staticmethod
     def unpack_int8(buf: BinaryIO) -> int:
-        return int.from_bytes(buf.read(1), byteorder='big', signed=False)
+        return Packet._unpack_int(buf, 1)
 
     @staticmethod
     def unpack_uint8(buf: BinaryIO) -> int:
-        return int.from_bytes(buf.read(1), byteorder='big', signed=True)
+        return Packet._unpack_int(buf, 1, False)
 
     @staticmethod
     def unpack_int16(buf: BinaryIO) -> int:
-        return int.from_bytes(buf.read(2), byteorder='big', signed=False)
+        return Packet._unpack_int(buf, 2)
 
     @staticmethod
     def unpack_uint16(buf: BinaryIO) -> int:
-        return int.from_bytes(buf.read(2), byteorder='big', signed=True)
+        return Packet._unpack_int(buf, 2, False)
 
     @staticmethod
     def unpack_int32(buf: BinaryIO) -> int:
-        return int.from_bytes(buf.read(4), byteorder='big', signed=True)
+        return Packet._unpack_int(buf, 4)
 
     @staticmethod
     def unpack_uint32(buf: BinaryIO) -> int:
-        return int.from_bytes(buf.read(4), byteorder='big', signed=False)
+        return Packet._unpack_int(buf, 4, False)
 
     @staticmethod
     def unpack_int64(buf: BinaryIO) -> int:
-        return int.from_bytes(buf.read(8), byteorder='big', signed=True)
+        return Packet._unpack_int(buf, 8)
 
     @staticmethod
     def unpack_uint64(buf: BinaryIO) -> int:
-        return int.from_bytes(buf.read(8), byteorder='big', signed=False)
+        return Packet._unpack_int(buf, 8, False)
 
     @staticmethod
     def unpack_string(buf: BinaryIO, length: int) -> str:
         raw = buf.read(length)
-
         return raw.strip(b'\x00').decode('utf-8')
+
+    @staticmethod
+    def _unpack_int(buf: Union[BinaryIO, BufferedIOBase], size: int, signed: bool = True) -> int:
+        return int.from_bytes(buf.read(size), byteorder='big', signed=signed)
