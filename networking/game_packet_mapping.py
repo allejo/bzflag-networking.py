28734ab5913508361084a775bf1fb07771b2e2e4
Vladimir Jimenez <allejo@me.com>
2019-01-20 23:52:06 -0800

    Add MsgCapture + MsgDropFlag packet support

--- networking/game_packet_mapping.py
+++ networking/game_packet_mapping.py
@@ -4,6 +4,8 @@ from networking.game_packet import GamePacket
 from networking.msg_add_player import MsgAddPlayerPacket
 from networking.msg_admin_info import MsgAdminInfoPacket
 from networking.msg_alive import MsgAlivePacket
+from networking.msg_capture_flag import MsgCaptureFlagPacket
+from networking.msg_drop_flag import MsgDropFlagPacket
 from networking.network_message import NetworkMessage
 
 
@@ -11,4 +13,6 @@ GamePacketMap: Dict[NetworkMessage, GamePacket] = {
     NetworkMessage.AddPlayer: MsgAddPlayerPacket(),
     NetworkMessage.AdminInfo: MsgAdminInfoPacket(),
     NetworkMessage.Alive: MsgAlivePacket(),
+    NetworkMessage.CaptureFlag: MsgCaptureFlagPacket(),
+    NetworkMessage.DropFlag: MsgDropFlagPacket(),
 }
