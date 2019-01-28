ed18ed129398440900e878445f2322b82f29e4d8
Vladimir Jimenez <allejo@me.com>
2019-01-27 21:18:39 -0800

    Add long description to setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 import setuptools
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='bzflag-networking',
     version='1.0.0',
     author='Vladimir "allejo" Jimenez',
     author_email='me@allejo.io',
     description='A Python library for reading and unpacking BZFlag network packets',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/allejo/bzflag-networking.py',
     packages=setuptools.find_packages(),
     classifiers={
