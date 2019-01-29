4a2cdc9b01decf59844cef3c0dd1eea0e736d94e
Vladimir Jimenez <allejo@me.com>
2019-01-28 23:54:33 -0800

    Add support for blacklist packet types in JSON

--- bzflag/utilities/json_encoder.py
+++ bzflag/utilities/json_encoder.py
@@ -1,11 +1,19 @@
 import json
 
+from typing import List
+
+from bzflag.networking.game_packet import GamePacket
 from bzflag.utilities.json_serializable import JsonSerializable
 
 
 class RRLogEncoder(json.JSONEncoder):
+    black_list: List[str] = []
+
     def default(self, obj):
         if not isinstance(obj, JsonSerializable):
+            return {}
+
+        if isinstance(obj, GamePacket) and obj.packet_type in RRLogEncoder.black_list:
             return {}
 
         return obj.to_dict()
